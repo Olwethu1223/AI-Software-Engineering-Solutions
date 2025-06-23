2c82df04045d2edce97cddf2625c30d7c0fcd973
Champez <sc21232332023@mut.ac.ke>
2025-06-23 08:55:26 +0300

    wrong push

--- model.py
+++ model.py
@@ -1,12 +1,1 @@
-mnist-cnn-classifier
-├── src
-│   ├── __init__.py
-│   ├── data_loading.py
-│   ├── model.py
-│   ├── train.py
-│   ├── evaluate.py
-│   └── utils
-│       ├── __init__.py
-│       └── visualization.py
-├── requirements.txt
-└── README.md
+
